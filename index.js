6667f93409fa2d7015511dd7ce1150927a45d4f7
Sean Christopher Febo <scf@techie.com>
2018-07-26 05:46:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function getIssues() {
+    fetch('https://api.github.com/repos/scfebo87/javascript-fetch-lab/issues')
+    .then(resp => resp.json())
+    .then(json => console.log(showIssues(json)));
 }
 
 function showIssues(json) {
+    $('#issues')[0].innerHTML += json;
 }
 
 function createIssue() {
+    const repo = 'https://api.github.com/repos/scfebo87/javascript-fetch-lab/issues'
+    const postData = {
+        title: document.getElementById('title').value, 
+        body: document.getElementById('body').value
+    };
+    fetch(repo, {
+        method: 'post',
+        body: JSON.stringify(postData),
+        headers: {
+            Authorization: `token ${getToken()}`
+        }
+    }).then(res => console.log(res));
 }
 
 function showResults(json) {
+    document.getElementById('results').innerHTML = `<a href="${json.html_url}">Forked Repo</a>`
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab'
+  fetch(repo, { 
+    method: 'post',
+    headers: {
+        Authorization: `token ${getToken()}`
+    }
+    }).then(res => res.json()).then(json => showResults(json)); 
   //use fetch to fork it!
 }
 
